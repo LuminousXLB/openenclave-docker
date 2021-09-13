d2d11bbb5abe37c3a5748b30fdba8f16ae9d9774
Shen Jiamin <shen_jiamin@comp.nus.edu.sg>
2021-09-13 13:55:59 +0000

    aesmd container
    Signed-off-by: Shen Jiamin <shen_jiamin@comp.nus.edu.sg>

---
+++ common/install-qcnl.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -xe
+
+apt-get install --no-install-recommends -y libsgx-dcap-default-qpl
+
+QCNL_CONF=/etc/sgx_default_qcnl.conf
+
+if [ $PCCS_SERVER ]
+then
+    sed "s/localhost:8081/"$PCCS_SERVER"/g" -i $QCNL_CONF
+fi
+
+sed "s/USE_SECURE_CERT=TRUE/USE_SECURE_CERT=FALSE/g" -i $QCNL_CONF
+
+cat $QCNL_CONF
